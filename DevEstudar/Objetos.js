998d5a75869296b0feabab1c2642e3b74ad3a327
brunochaves77 <68792408+brunochaves77@users.noreply.github.com>
2020-08-02 01:31:26 -0300

    Mais estudos

---
+++ DevEstudar/Objetos.js
@@ -0,0 +1,13 @@
+let pessoa = {
+    nome: 'Daniel',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de souza'
+};
+let pet = {
+    nome: 'Canario',
+    idade: 2,
+    sexo: 'macho'
+};
+console.log(pessoa);
+console.log(pet);
