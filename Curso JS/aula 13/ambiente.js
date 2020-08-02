459204a67ecf54aecea2c30ee84d055acadda653
brunochaves77 <68792408+brunochaves77@users.noreply.github.com>
2020-08-02 17:49:51 -0300

    Aprendendo Repetições

---
+++ Curso JS/aula 13/ambiente.js
@@ -0,0 +1,6 @@
+var d = 0
+do {
+    console.log(`Passo ${d}`)  
+    d++  
+} while (d <= 60)
+
---
+++ Curso JS/aula 14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar ...')
+for (var s = 1; s <= 4; s++) {
+    console.log(s)
+}
+console.log('FIM')
