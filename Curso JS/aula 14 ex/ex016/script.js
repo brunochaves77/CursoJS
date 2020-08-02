acf4893669fd8b7321530e2e1fe044b31682e67d
brunochaves77 <68792408+brunochaves77@users.noreply.github.com>
2020-08-02 19:27:30 -0300

    Exercicio 17

--- Curso JS/aula 14 ex/ex016/script.js
+++ Curso JS/aula 14 ex/ex016/script.js
@@ -5,12 +5,17 @@ function contarPasso() {
     let res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
         window.alert('[ERRO] Insira um numero valido!')
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: </br>'
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Invalido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             //Contagem crescente
             for (let c = i; c <= f; c += p) {
--- Curso JS/aula 14 ex/ex017/script.js
+++ Curso JS/aula 14 ex/ex017/script.js
@@ -1,1 +1,5 @@
-alert('Ola!')
+function gerarT() {
+    let numero = window.document.getElementById('txtn')
+}if (numero = 0) {
+    window.alert('ERRO')
+}
